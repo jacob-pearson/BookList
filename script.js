86418911730dc2f7423ddead5b5bd55ff31b3d66
jacob-pearson <jcbpearson09@gmail.com>
2022-10-28 13:43:30 -0500

    setsup the base javascript file and links it to index.html

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-console.log('hello from the other side')
+let bookList = []
+
+function Book(title, author, pages){
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.status = ''
+}
+
+function addBookToLibrary(book, bookList){
+    bookList.push(book)
+}
+
+let thehobbit = new Book('The Hobbit', 'Tolkien', '295')
+
+console.log(thehobbit)
+addBookToLibrary(thehobbit, bookList)
+console.log(bookList)
+
+function displayBooks(){
+
+
+}
+
+function deleteBook(){
+
+
+}
+
+Book.prototype.read = function(){
+
+
+}
+
+Book.prototype.unread = function(){
+
+
+}
