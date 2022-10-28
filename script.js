b601df68e81412a8865dad0abf064486272c8d3e
jacob-pearson <jcbpearson09@gmail.com>
2022-10-28 15:04:44 -0500

    moved the displaybooks function to the end of the javascript file

--- script.js
+++ script.js
@@ -17,10 +17,6 @@ let thehobbit = new Book('The Hobbit', 'Tolkien', '295')
 let theHobbit2 = new Book('the hobbit 2', 'Tolkien', '296')
 addBookToLibrary(thehobbit, bookList)
 addBookToLibrary(theHobbit2, bookList)
-function displayBooks(){
-
-
-}
 
 function deleteBook(bookIndex, bookList){
     bookList.splice(bookList[bookIndex], 1)
@@ -29,17 +25,21 @@ function deleteBook(bookIndex, bookList){
 
 Book.prototype.read = function(){
     this.status = 'read'
-    return this.status
+
 
 
 }
 
 Book.prototype.unread = function(){
     this.status = 'unread'
-    return this.status
+
 }
-theHobbit2.unread()
-thehobbit.read()
-console.log(bookList)
+
+function displayBooks(bookList){
+    
+
+
+}
+
 
 
